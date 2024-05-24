8a7b73fdd9b11796d6c11b1bfb5bb0f3f3034d39
anassezzine <ezzin.anass@gmail.com>
2024-05-24 12:02:13 +0200

    classe utilisateur

---
+++ src/main/java/com/projets/model/Utilisateur.java
@@ -0,0 +1,50 @@
+package com.projets.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance (strategy = InheritanceType.JOINED)
+public class Utilisateur {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	 
+	private String name;
+	private String email;
+	public Utilisateur(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	 
+	 
+	 
+	 
+	
+
+}
