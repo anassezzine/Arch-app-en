a367101936082cc7f349a98a980a22a7aefc01af
anassezzine <ezzin.anass@gmail.com>
2024-05-24 11:07:31 +0200

    First commit

---
+++ src/main/java/com/projets/GestionDevLogicielApplication.java
@@ -0,0 +1,13 @@
+package com.projets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDevLogicielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDevLogicielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projets/GestionDevLogicielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDevLogicielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
